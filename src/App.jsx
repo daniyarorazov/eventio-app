c375de6df86babbac65a3100a3de272ef30c97d3
Daniyar Orazov <orazov.video@gmail.com>
2023-03-29 21:45:56 +0200

    Added function to add new some field of guests in CreateEventView

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import './App.css'
-import EventPage from "./views/EventPage.jsx";
 
-import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getPerformance } from "firebase/performance";
-import { getStorage } from "firebase/storage";
 
 import {useAuthState} from "react-firebase-hooks/auth";
-import {useCollectionData}  from "react-firebase-hooks/firestore";
 
 import {app, auth, firestore, performance, storage} from "./db";
-
-
-
-
 
 function App() {
     const [user] = useAuthState(auth)
--- src/views/CreateEventView.jsx
+++ src/views/CreateEventView.jsx
@@ -1,19 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import { collection, addDoc } from "firebase/firestore";
+import {collection, addDoc, doc} from "firebase/firestore";
 import {app, auth, firestore, performance, storage} from "../db";
 import './CreateEventView.css';
 import InputField from "../components/InputField.jsx";
 import Button from "../components/Button.jsx";
 import {Link, useNavigate} from "react-router-dom";
+
 const CreateEventView = () => {
     const [uid, setUid] = useState(null);
     const db = firestore;
     const [nameEvent, setNameEvent] = useState('');
     const [date, setDate] = useState('');
-    const [guest, setGuest] = useState('');
-    const [guestProject, setGuestProject] = useState('');
+    const [guests, setGuests] = useState([
+        { id: 1, name: "", project: "" },
+    ]);
+
     const [docID, setDocID] = useState('');
     const navigate = useNavigate();
+    const [numFields, setNumFields] = useState(1);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -33,21 +37,46 @@ const CreateEventView = () => {
             const docRef = await addDoc(collection(db, 'events'), {
                 title: nameEvent,
                 date: date,
-                guest: guest,
-                guestProject: guestProject,
                 uid: uid
                 // any other event details you want to add
+            });
+            guests.forEach(async (guest) => {
+                await addDoc(collection(doc(db, 'events', docRef.id), 'guests'), guest);
             });
             console.log('Document written with ID: ', docRef.id);
             setDocID(docRef.id)
             setNameEvent('');
             setDate('');
-            setGuest('');
-            setGuestProject('');
             navigate(`/share/${docRef.id}`);
         } catch (error) {
             console.error('Error adding document: ', error);
         }
+    };
+
+    const handleAddNewField = () => {
+        setNumFields(numFields + 1);
+        setGuests([...guests, { id: numFields + 1, name: "", project: "" }]);
+    };
+
+
+    const handleNameChange = (id, value) => {
+        setGuests(guests.map((guest) => {
+            if (guest.id === id) {
+                return { ...guest, name: value };
+            } else {
+                return guest;
+            }
+        }));
+    };
+
+    const handleProjectChange = (id, value) => {
+        setGuests(guests.map((guest) => {
+            if (guest.id === id) {
+                return { ...guest, project: value };
+            } else {
+                return guest;
+            }
+        }));
     };
 
 
@@ -68,19 +97,33 @@ const CreateEventView = () => {
                 valueLabel="Date of event"
                 className="form-block__input"
             />
-            <InputField
-                type="text"
-                valueInput={guest}
-                onChange={(e) => setGuest(e.target.value)}
-                valueLabel="Guest name"
-                className="form-block__input"
+            {guests.map((guest) => (
+                <div key={guest.id}>
+                    <InputField
+                        type="text"
+                        valueInput={guest.name}
+                        onChange={(e) => handleNameChange(guest.id, e.target.value)}
+                        valueLabel={`Guest name (${guest.id})`}
+                        className="form-block__input"
+                    />
+                    <InputField
+                        type="text"
+                        valueInput={guest.project}
+                        onChange={(e) => handleProjectChange(guest.id, e.target.value)}
+                        valueLabel={`Guest project (${guest.id})`}
+                        className="form-block__input"
+                    />
+                </div>
+            ))}
+            <Button
+                onClick={handleAddNewField}
+                value={"+"}
+                className="form-block__button"
             />
-            <InputField
-                type="text"
-                valueInput={guestProject}
-                onChange={(e) => setGuestProject(e.target.value)}
-                valueLabel="Guest project"
-                className="form-block__input"
+            <Button
+                onClick={() => console.log(guests)}
+                value={"print"}
+                className="form-block__button"
             />
             <Button
                 onClick={handleSubmit}
